49664c01ce9339b22fa461f70c45af5bd8e64633
TIMI-gns <babalolae502@gmail.com>
2025-11-09 16:45:11 +0100

    Add files via upload

---
+++ TIMI.js
@@ -0,0 +1,7 @@
+const name = "BABALOLA TIMILEYIN EMMANUEL";
+
+let age = 17;
+
+let favProgLang = "Javascript";
+
+console.log("Hello there👋, I go by the name " + name + ", I'm " + age + "years old,and my best programming language is " + favProgLang +".. Thanks for having me.")
